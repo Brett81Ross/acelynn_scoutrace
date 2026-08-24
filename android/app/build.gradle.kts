971b9ac45e6c1d3b756a7c4b602390f368f8d782
Brett  Ross <rossbrett05@gmail.com>
2026-08-24 09:39:01 -0500

    Align Android Java and Kotlin JVM targets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,23 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
 android {
     namespace = "com.cactusbyte.scouttrace"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.cactusbyte.scouttrace"
         minSdk = 26
         targetSdk = 35
         versionCode = 20000
         versionName = "2.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
